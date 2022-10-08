f72f496e9b214417b0cd56a3a9ebffc96ee8bd9d
Azazel-Woodwind <azazelwoodwind11@gmail.com>
2022-10-09 00:23:35 +0100

    ez

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
 def primes(number_of_primes):
-    list = []
+    if (number_of_primes <= 0):
+        raise ValueError
+        
+    list = [2]
+    candidate = 3
+    while (len(list) < number_of_primes):
+        i = 0
+        isPrime = True
+        while (list[i] <= math.sqrt(candidate)):
+            if (not (candidate % list[i])):
+                isPrime = False
+                break
+            i += 1
+
+        if (isPrime):
+            list.append(candidate)
+
+        candidate += 2
+
     return list
